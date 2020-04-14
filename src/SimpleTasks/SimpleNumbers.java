d0972714a8aff448ca408b1909eccd2f710f0414
omalynovskyy <oleksiy.malynovskyy@gmail.com>
2020-04-14 23:59:40 +0300

    SimpleNumbers

--- src/SimpleTasks/SimpleNumbers.java
+++ src/SimpleTasks/SimpleNumbers.java
@@ -6,18 +6,13 @@ public class SimpleNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int max = scanner.nextInt();
-        for(int i=1;(i <= max)&&(i<=4); i++){
-            if(i==4){
-                continue;
-            }
+        for(int i=1;(i <= max)&&(i<9); i+=2){
             System.out.print(i+"; ");
          }
-        for(int i=5;(i <= max)&&(i<9); i+=2){
-            System.out.print(i+"; ");
-        }
+
         for(int i=9;i<=max;i+=2){
             int check = i%3*i%5*i%7*i%9;
-            if (check==0) {
+            if ((check==0)|(Math.sqrt(i)%1==0)) {
                 continue;
             }
             System.out.print(i+"; ");
