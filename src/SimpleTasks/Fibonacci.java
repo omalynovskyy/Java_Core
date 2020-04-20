b2fe55d95d3df00b272b06fb165c191f1898cee6
omalynovskyy <oleksiy.malynovskyy@gmail.com>
2020-04-20 20:10:38 +0300

    SimpleNumbers

--- src/SimpleTasks/Fibonacci.java
+++ src/SimpleTasks/Fibonacci.java
@@ -8,19 +8,25 @@ public class Fibonacci {
         int num = scanner.nextInt();
         int start = 1;
         int next = 1;
-        switch (num){
-            case (1):
-                System.out.println(start);
-            case (2):
-                System.out.println(next);
-            default:{
-                int result=1;
-                for(int i=3; i<=num;i++){
-                    start = next;
-                    next = result;
-                    result = next+start;
+        if(num < 1){
+            System.out.println("number should be > 0");
+        } else {
+            switch (num) {
+                case (1):
+                    System.out.println(start);
+                    break;
+                case (2):
+                    System.out.println(next);
+                    break;
+                default: {
+                    int result = 1;
+                    for (int i = 3; i <= num; i++) {
+                        start = next;
+                        next = result;
+                        result = next + start;
+                    }
+                    System.out.println(result);
                 }
-                System.out.println(result);
             }
         }
     }
