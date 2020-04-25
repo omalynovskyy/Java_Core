21fb69cdeccf0354a9ff2758c61f669fe8a1d1a7
omalynovskyy <oleksiy.malynovskyy@gmail.com>
2020-04-26 00:18:12 +0300

    CardGamesStart

--- src/CardGames/Deck.java
+++ src/CardGames/Deck.java
@@ -1,26 +1,41 @@
 package CardGames;
 
-import java.util.Arrays;
+import javax.swing.*;
+import java.util.Random;
 
 public class Deck {
     private int value;
-    Card[] emptyDeck = new Card[36];
+    Card[] startDeck = new Card[36];
 
     void newDeck() {
         value = 1;
-        for (int i = 0; i < emptyDeck.length; ) {
+        for (int i = 0; i < startDeck.length; ) {
             for (Suite suite: Suite.values()) {
-                emptyDeck[i++] = new Card(suite, value);
+                startDeck[i++] = new Card(suite, value);
             }
             value++;
         }
     }
 
+    void mixDeck(){
+        Random random = new Random();
+        int cardNum = 0;
+        for(int i = 0; i < startDeck.length; i++){
+            if(i == cardNum){
+                cardNum = random.nextInt(36);
+            }
+        }
+
+
+
+
+    }
+
     @Override
     public String toString() {
         String deckCards = "";
-        for(int i = 0; i < emptyDeck.length; i++){
-            deckCards += emptyDeck[i].getPower() + " & " + emptyDeck[i].getSuite() + "; ";
+        for(int i = 0; i < startDeck.length; i++){
+            deckCards += startDeck[i].getPower() + " & " + startDeck[i].getSuite() + "; ";
         }
         return deckCards;
     }
