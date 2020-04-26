809aea5e5a118ee6d6399ebf9420cca21867cc50
omalynovskyy <oleksiy.malynovskyy@gmail.com>
2020-04-27 00:02:08 +0300

    CardGamesStart

--- src/CardGames/Game1.java
+++ src/CardGames/Game1.java
@@ -4,10 +4,7 @@ public class Game1 {
     static Deck deck = new Deck();
     public static void main(String[] args) {
         deck.newDeck();
-        System.out.println(deck.toString());
         deck.mixDeck();
-        System.out.println(deck.toString());
-        deck.mixDeck();
-        System.out.println(deck.toString());
+
     }
 }
