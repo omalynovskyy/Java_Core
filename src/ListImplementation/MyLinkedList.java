7e9a0ca7928e766ca3f14ca954f7baf955632d85
omalynovskyy <oleksiy.malynovskyy@gmail.com>
2020-07-06 03:01:35 +0300

    MyLinkedList

--- src/ListImplementation/MyLinkedList.java
+++ src/ListImplementation/MyLinkedList.java
@@ -2,7 +2,7 @@ package ListImplementation;
 
 import java.util.*;
 
-public class MyLinkedList implements List {
+public class MyLinkedList<E> implements List {
 
     Collection<ListElement> elements;
     ListElement firstElement;
@@ -56,8 +56,8 @@ public class MyLinkedList implements List {
 
     @Override
     public Iterator iterator() {
-        Iterator<ListElement> iterator = new Iterator<>() {
-            ListElement currentElement = firstElement;
+        Iterator<E> iterator = new Iterator<E>() {
+            ListElement<E> currentElement = firstElement;
             @Override
             public boolean hasNext() {
                 if (currentElement.nextElement == null) {return false;}
@@ -65,11 +65,11 @@ public class MyLinkedList implements List {
             }
 
             @Override
-            public ListElement next() {
+            public E next() {
                 currentElement = currentElement.nextElement;
                 if (currentElement == null) {
                     throw new NoSuchElementException();
-                } else {return currentElement;}
+                } else {return currentElement.element;}
             }
         };
         return iterator;
@@ -89,54 +89,96 @@ public class MyLinkedList implements List {
 
     @Override
     public boolean add(Object o) {
-        return false;
+        ListElement newElement = new ListElement((E) o);
+        if (this.size() == 0) {
+            firstElement = newElement;
+        } else {
+            lastElement.nextElement = newElement;
+            newElement.previousElement = lastElement;
+        }
+        lastElement = newElement;
+        elements.add(newElement);
+
+        return true;
     }
 
     @Override
     public boolean remove(Object o) {
-        return false;
+        ListElement e = firstElement;
+        while (!e.element.equals(o)){
+            if (e.nextElement == null) {return false;}
+            e = e.nextElement;
+        }
+        if (e == firstElement) {
+            firstElement = e.nextElement;
+            e.nextElement.previousElement = null;
+        } else if (e == lastElement) {
+            lastElement = e.previousElement;
+            e.previousElement.nextElement = null;
+        } else {
+        e.previousElement.nextElement = e.nextElement;
+        e.nextElement.previousElement = e.previousElement;
+        }
+        // не уверен или так можно: "e.nextElement.previousElement", тогда альтернативой наверное через промежуточные переменные?
+        elements.remove(e);
+        return true;
     }
 
     @Override
     public boolean addAll(Collection c) {
-        return false;
+        boolean result = false;
+        for (Object o : c) {
+            result = this.add(o);
+        }
+        return result;
     }
 
+    // учитывая что этот лист не содержит индексов то этот метод переопределать не нужно?
     @Override
     public boolean addAll(int index, Collection c) {
         return false;
     }
 
+    // вот не уверен или такой подход правильный, но ты мне скажи )); альтернативой может бить объявление новой коллекции
     @Override
     public void clear() {
+        for (ListElement e : elements) {
+            elements.remove(e);
+        }
 
     }
 
+    // учитывая что этот лист не содержит индексов то этот метод переопределать не нужно?
     @Override
     public Object get(int index) {
         return null;
     }
 
+    // учитывая что этот лист не содержит индексов то этот метод переопределать не нужно?
     @Override
     public Object set(int index, Object element) {
         return null;
     }
 
+    // учитывая что этот лист не содержит индексов то этот метод переопределать не нужно?
     @Override
     public void add(int index, Object element) {
 
     }
 
+    // учитывая что этот лист не содержит индексов то этот метод переопределать не нужно?
     @Override
     public Object remove(int index) {
         return null;
     }
 
+    // учитывая что этот лист не содержит индексов то этот метод переопределать не нужно?
     @Override
     public int indexOf(Object o) {
         return 0;
     }
 
+    // учитывая что этот лист не содержит индексов то этот метод переопределать не нужно?
     @Override
     public int lastIndexOf(Object o) {
         return 0;
@@ -144,9 +186,99 @@ public class MyLinkedList implements List {
 
     @Override
     public ListIterator listIterator() {
-        return null;
+        ListIterator<E> listIterator = new ListIterator<E>() {
+            ListElement<E> currentElement = firstElement;
+            E currentObject = currentElement.element;
+            boolean removeAllowed = false;
+
+            @Override
+            public boolean hasNext() {
+                if (currentElement.nextElement == null) {return false;}
+                return true;
+            }
+
+            @Override
+            public E next() {
+                currentElement = currentElement.nextElement;
+                if (currentElement == null) {
+                    throw new NoSuchElementException();
+                } else {
+                    removeAllowed = true;
+                    return currentElement.element;
+                }
+            }
+
+
+            @Override
+            public boolean hasPrevious() {
+                if (currentElement.previousElement == null) {return false;}
+                return true;
+            }
+
+            @Override
+            public E previous() {
+                currentElement = currentElement.previousElement;
+                if (currentElement == null) {
+                    throw new NoSuchElementException();
+                } else {
+                    removeAllowed = true;
+                    return currentElement.element;
+                }
+            }
+
+            // учитывая что этот лист не содержит индексов то этот метод переопределать не нужно?
+            @Override
+            public int nextIndex() {
+                return 0;
+            }
+
+            // учитывая что этот лист не содержит индексов то этот метод переопределать не нужно?
+            @Override
+            public int previousIndex() {
+                return 0;
+            }
+
+            @Override
+            public void remove() {
+                if (removeAllowed) {
+                    if (currentElement == firstElement) {
+                        firstElement = currentElement.nextElement;
+                        currentElement.nextElement.previousElement = null;
+                        currentElement = firstElement;
+                    } else if (currentElement == lastElement) {
+                        lastElement = currentElement.previousElement;
+                        currentElement.previousElement.nextElement = null;
+                        currentElement = lastElement;
+                    } else {
+                        currentElement.previousElement.nextElement = currentElement.nextElement;
+                        currentElement.nextElement.previousElement = currentElement.previousElement;
+                        currentElement = currentElement.nextElement;
+                    }
+                    elements.remove(currentElement);
+                    removeAllowed = false;
+                }
+
+            }
+
+            @Override
+            public void set(E e) {
+                currentElement.element = e;
+            }
+
+            @Override
+            public void add(E e) {
+                ListElement<E> newElement = new ListElement<>(e);
+                currentElement.previousElement.nextElement = newElement;
+                newElement.previousElement = currentElement.previousElement;
+                newElement.nextElement = currentElement;
+                currentElement.previousElement = newElement;
+
+            }
+        };
+        return listIterator;
     }
 
+    // учитывая что этот лист не содержит индексов то этот метод переопределать не нужно?
     @Override
     public ListIterator listIterator(int index) {
         return null;
